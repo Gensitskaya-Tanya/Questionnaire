a5dae62063f1e9e11ad694a34675e07141c9132d
Gensitskaya_Tanya <gensitskaya.tanya@gmail.com>
2016-11-23 15:46:59 +0200

    anketa

---
+++ src/ua/kiev/prog/QuestionServlet.java
@@ -0,0 +1,35 @@
+package ua.kiev.prog;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+
+public class QuestionServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+
+		String name = req.getParameter("name");
+		String surname = req.getParameter("surname");
+		String age = req.getParameter("age");
+		String questionOne = req.getParameter("question-one");
+		String questionTwo = req.getParameter("question-two");
+
+		String correctAnswer1 = "b1 = -128";
+		String correctAnswer2 = "Будет напечатано TEST TEST  без кавычек";
+		String title = surname + " " + name + ", ваш результат:";
+		String msg ="";
+		if(questionOne!=null && questionTwo!=null) {
+			if (questionOne.equals(correctAnswer1) && questionTwo.equals(correctAnswer2)) {
+				msg = title + "<br> " + "вы ответили правильно на все вопросы";
+			} else if (questionOne.equals(correctAnswer1) || questionTwo.equals(correctAnswer2)) {
+				msg = title + "<br> " + "вы ответили правильно на один вопрос";
+			} else {
+				msg = title + " <br>" + "нет правильных ответов";
+			}
+		}
+
+		resp.getWriter().println(msg);
+	}
+}
